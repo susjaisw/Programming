df89959ba7671057168e51a32c4730c16b3c1927
-v <rohannie456@gmail.com>
2023-02-15 22:52:24 +0530

    Binary Search using iteration

---
+++ Binary_search.cpp
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int Binary_Search(int [],int,int);
+int main()
+{
+int n ,k;
+int a[100];
+printf("Enter the size of array: \n");
+scanf("%d",&n);
+
+printf("Enter the element of array: \n");
+for(int i=0;i<n;i++)
+scanf("%d",&a[i]);
+
+printf("Enter the element to be searched: \n");
+scanf("%d",&k);
+
+
+int Result= Binary_Search(a,n,k);
+if(Result==-1)
+printf("element not found\n");
+else printf("element found at index : %d\n",Result);
+return 0;
+
+}
+
+int Binary_Search(int array[],int n,int k)
+{
+    int low=0 , high=n-1 , mid=0;
+
+    while(low<=high)
+    {
+    mid=(low+high)/2;
+
+    if(array[mid]==k)
+    return mid;
+
+    if(array[mid]<k)
+    low=mid+1;
+
+    else high=mid-1;
+    }
+    return -1;
+}
